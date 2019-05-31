83349f7052ab132adad387e2a4a951ca8b411a3b
armfazh <armfazh@cloudflare.com>
2019-05-31 01:00:01 -0700

    Including RFC8032 test vectors.

---
+++ sign/ed25519/rfc8032_test.go
@@ -0,0 +1,73 @@
+package ed25519
+
+import (
+	"archive/zip"
+	"bufio"
+	"encoding/hex"
+	"fmt"
+	"github.com/cloudflare/circl/internal/test"
+	"strings"
+	"testing"
+)
+
+func processRow(t *testing.T, line string, lineNum int) {
+	var public Pk25519
+	var scheme Ed25519
+	var v struct {
+		secret    Sk25519
+		public    Pk25519
+		message   []byte
+		signature Sig25519
+	}
+	values := strings.Split(line, ":")
+	if len(values) != 5 {
+		panic(fmt.Errorf("len: %v %v\n", len(values), values))
+	}
+	b, _ := hex.DecodeString(values[0])
+	copy(v.secret[:], b[:32])
+	b, _ = hex.DecodeString(values[1])
+	copy(v.public[:], b)
+	v.message, _ = hex.DecodeString(values[2])
+	b, _ = hex.DecodeString(values[3])
+	copy(v.signature[:], b[:64])
+
+	scheme.KeyGen(&public, &v.secret)
+	if public != v.public {
+		got := public
+		want := v.public
+		test.ReportError(t, got, want, lineNum, v)
+	}
+	signature := scheme.Sign(v.message, &v.public, &v.secret)
+	if *signature != v.signature {
+		got := *signature
+		want := v.signature
+		test.ReportError(t, got, want, lineNum, v)
+	}
+	got := scheme.Verify(v.message, &v.public, &v.signature)
+	want := true
+	if got != want {
+		test.ReportError(t, got, want, lineNum, v)
+	}
+}
+
+func TestRFC8032(t *testing.T) {
+	const nameFile = "testdata/sign.input.zip"
+	zipFile, err := zip.OpenReader(nameFile)
+	if err != nil {
+		t.Fatalf("File %v can not be opened. Error: %v", nameFile, err)
+	}
+	defer zipFile.Close()
+
+	for _, f := range zipFile.File {
+		unzipped, err := f.Open()
+		if err != nil {
+			t.Fatalf("File %v can not be opened. Error: %v", f.Name, err)
+		}
+		defer unzipped.Close()
+
+		fScanner := bufio.NewScanner(unzipped)
+		for i := 1; fScanner.Scan(); i++ {
+			processRow(t, fScanner.Text(), i)
+		}
+	}
+}
